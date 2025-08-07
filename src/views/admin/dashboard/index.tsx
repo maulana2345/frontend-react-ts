aabec05c1ddba38db4c5afcdf0d7e4fe53642bb5
maulana2345 <naufalimal171@gmail.com>
2025-08-07 15:37:36 +0700

    first commit

---
+++ src/views/admin/dashboard/index.tsx
@@ -0,0 +1,29 @@
+//import FC from react
+import { FC } from "react";
+
+//import SidebarMenu
+import SidebarMenu from '../../../components/SidebarMenu';
+
+const Dashboard: FC = () => {
+    return (
+        <div className="container mt-5 mb-5">
+            <div className="row">
+                <div className="col-md-3">
+                    <SidebarMenu />
+                </div>
+                <div className="col-md-9">
+                    <div className="card border-0 rounded-4 shadow-sm">
+                        <div className="card-header">
+                            DASHBOARD
+                        </div>
+                        <div className="card-body">
+                            Selamat Datang, <strong></strong>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard
